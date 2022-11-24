2b54f660f487f431c4accdaad2e47b6f41dbb769
Jonah Park <jonahp1234@gmail.com>
2022-11-24 02:15:09 +0000

    prep-javascript-for-loops -Jonah Park

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+for (let num = 0; num < 10; num++) {
+  console.log(num);
+}
+
+console.log('LINE BREAK BETWEEN CODE');
+
+for (let num2 = 0; num2 < 20; num2 += 2) {
+  console.log(num2);
+}
+
+console.log('LINE BREAK BETWEEN CODE');
+
+for (let num100s = 100; num100s >= 0; num100s -= 1) {
+  console.log('Time till explosion:', num100s);
+}
